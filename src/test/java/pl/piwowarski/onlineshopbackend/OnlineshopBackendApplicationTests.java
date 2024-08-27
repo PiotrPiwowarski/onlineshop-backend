61692b3ac49b1014a72d47cb83aa0b76e39c4afe
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-08-27 12:44:42 +0200

    initial commit

---
+++ src/test/java/pl/piwowarski/onlineshopbackend/OnlineshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piwowarski.onlineshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
